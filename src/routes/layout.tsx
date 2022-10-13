bcc6898694f5d581c0c237992ee8496bfd397ba5
FMGordillo <facundomgordillo@gmail.com>
2022-10-13 23:00:39 +0200

    feat: added my own design (wip)

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,19 +1,18 @@
 import { component$, Slot } from '@builder.io/qwik';
-import Header from '../components/header/header';
 
 export default component$(() => {
   return (
     <>
       <main>
-        <Header />
         <section>
           <Slot />
         </section>
       </main>
-      <footer>
+      <footer className="flex gap-8 h-14 justify-center place-items-center bg-indigo-100">
         <a href="https://www.builder.io/" target="_blank">
           Made with ♡ by Builder.io
         </a>
+        <a href="https://github.com/FMGordillo" target="_blank">Modified by Facundo Martin Gordillo</a>
       </footer>
     </>
   );
